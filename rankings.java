17bc2fcca3b22ef0542557d0622fc9463430ea24
Sachleen <97253957+sachT19@users.noreply.github.com>
2022-07-13 23:03:13 +0300

    Create rankings.java

---
+++ rankings.java
@@ -0,0 +1,46 @@
+public class TeamRanks
+{
+    public static void main(String[] args) {
+		String team1 = "Alabama";
+		String team2 = "Ohio State";
+		String team3 = "Florida State";
+		String team4 = "USC";
+		String team5 = "Clemson";
+		String team6 = "Penn State";
+		String team7 = "Oklahoma";
+		String team8 = "Maryland";
+		String team9 = "Wisconsin";
+		String team10 = "Michigan";
+	
+    team1 = team1;
+    team10 = team2;
+    team2 = team6;
+    team6 = team5;
+    team5 = team4;
+    team4 = "Michigan";
+    team9 = team3;
+    team3 = team8;
+    team8 = "Wisconsin";
+        
+		System.out.print("1. ");
+		System.out.println(team1);
+		System.out.print("2. ");
+		System.out.println(team2);
+		System.out.print("3. ");
+		System.out.println(team3);
+		System.out.print("4. ");
+		System.out.println(team4);
+		System.out.print("5. ");
+		System.out.println(team5);
+		System.out.print("6. ");
+		System.out.println(team6);
+		System.out.print("7. ");
+		System.out.println(team7);
+		System.out.print("8. ");
+		System.out.println(team8);
+		System.out.print("9. ");
+		System.out.println(team9);
+		System.out.print("10. ");
+		System.out.println(team10);
+	}
+}
